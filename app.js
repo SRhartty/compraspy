670ca83ecf9d2c5c3da352965b42ca086d83e1e2
SRhartty <randonsspace@gmail.com>
2024-05-18 09:43:25 -0400

    index

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ cron.schedule('*/10 * * * * *', () => {
 
 
 var priceRouter = require('./routes/link');
+var indexRouter = require('./routes/index');
 
 var app = express();
 
@@ -22,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/link', priceRouter);
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
