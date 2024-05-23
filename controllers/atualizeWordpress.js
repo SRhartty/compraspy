630199c6273fa7f50e4d15e752991cd8fa764ba8
SRhartty <randonsspace@gmail.com>
2024-05-22 21:19:59 -0300

    fix

--- controllers/atualizeWordpress.js
+++ controllers/atualizeWordpress.js
@@ -3,14 +3,13 @@ const woocommerce = require('../exports/woocommerce');
 //process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 exports.atualizeWordpress = async (regular_price, id_produto) => {
-     
-   woocommerce.WooCommerce.put(`products/${parseFloat(id_produto)}`, {
+     console.log(typeof regular_price , regular_price);
+   woocommerce.WooCommerce.put(`products/${id_produto}`, {
          regular_price: regular_price
     }, function(err, data, res) {
          if (err) {
               console.log(err);
               return;
          }
-         console.log(res);
     });
 }
